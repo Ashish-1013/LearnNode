0edcf36935d88b38bb741b97d9c3d7a5699921c2
A K Giri <126046055+Ashish-1013@users.noreply.github.com>
2025-12-29 17:22:45 +0530

    Fix server log URL from HTTPS to HTTP

--- Server1.js
+++ Server1.js
@@ -7,4 +7,5 @@ const server = http.createServer((req, res) => {console.log(req)})
 const PORT = 30001;
 
 
-server.listen(PORT , ()=>{console.log(`Server running at https://localhost:${PORT}`)})
+server.listen(PORT , ()=>{console.log(`Server running at http://localhost:${PORT}`)})
+
